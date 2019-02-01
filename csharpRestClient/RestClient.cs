962a994295108b87b434bbb633de985ee413eee5
Andrew Coleman <andrew.coleman330@gmail.com>
2019-02-01 10:45:29 -0600

    commiting for testing

---
+++ csharpRestClient/RestClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace csharpRestClient
+{
+
+    public enum httpVerb
+    {
+        GET,
+        PUT,
+        POST,
+        DELETE
+    }
+    class RestClient
+    {
+        public string endPoint { get; set; }
+
+        public httpVerb httpMethod { get; set; }
+
+        public RestClient()     //purposeful constructor.
+        {
+            endPoint = string.Empty;
+            httpMethod = httpVerb.GET;
+        }
+
+        public string makeRequest()
+        {
+
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = httpMethod.ToString();
+
+            HttpWebResponse response = null;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+
+
+                //Proecess the resppnse stream... (could be JSON, XML or HTML etc..._
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                strResponseValue = "{\"errorMessages\":[\"" + ex.Message.ToString() + "\"],\"errors\":{}}";
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    ((IDisposable)response).Dispose();
+                }
+            }
+
+            return strResponseValue;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
